17c6d809c694a34781d41323f7d6000358924812
Matt Taddy <taddy@chicagobooth.edu>
2013-11-18 08:56:58 -0600

    wls compare

--- code/bootfwd.R
+++ code/bootfwd.R
@@ -22,6 +22,7 @@ geteffects <- function(b){
 	YI <- Y[BI[[b]]]
 	system.time(fwd <- glm(YI ~ ., data=VMZ, family="poisson"))
 	beta <- coef(fwd)
+	print(b)
 	return(beta[dvar])
 }
 
@@ -36,11 +37,18 @@ write.table(alpha, "results/treatments.txt",
 
 alpha <- read.table("results/treatments.txt", sep="|", header=TRUE)
 
+glmm <- c(0.0581505, 0.1448512, 0.1050877, 0.3363976,-0.1648610)
+glms <- c(0.0017694398,0.0008040118,0.0004546722,0.0022478543,0.0020281476)
+names(glmm) <- names(glms) <- 
+	c("usr.stars","usr.count","usr.funny","usr.useful","usr.cool")
+
 pdf("graphs/boots.pdf",width=8,height=2)
 par(mfrow=c(1,5), mai=c(0.4,.4,0.2,0.2),omi=c(.2,.2,.1,0))
 for(d in colnames(alpha)){
 	hist(alpha[,d],col="grey60",border="grey90",breaks=4,
 		bty="n",ylab="",xlab="", main=d,freq=FALSE)
+	polygon(x = glmm[d] + c(-2,-2,2,2)*glms[d], y=c(0,1e9,1e9,0), 
+		border=FALSE, col=rgb(255,165,0,150,max=255))
 }
 mtext("coefficient",side=1,font=3,outer=TRUE)
 mtext("density",side=2,font=3,outer=TRUE)
